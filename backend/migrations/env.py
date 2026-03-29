64bf7799302f2dbc9507dccb060d7bef6986de72
gdyshi <gdyshi@126.com>
2026-03-29 20:16:43 +0800

    fix: alembic 使用 DATABASE_URL 环境变量

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -1,8 +1,7 @@
+import os
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
+from sqlalchemy import engine_from_config, pool
 from alembic import context
 
 # this is the Alembic Config object, which provides
@@ -21,19 +20,14 @@ if config.config_file_name is not None:
 target_metadata = None
 
 
+def get_url():
+    """Get database URL from environment or fallback to config file."""
+    return os.environ.get('DATABASE_URL') or config.get_main_option("sqlalchemy.url")
+
+
 def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode.
-
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
-
-    Calls to context.execute() here emit the given string to the
-    script output.
-
-    """
-    url = config.get_main_option("sqlalchemy.url")
+    """Run migrations in 'offline' mode."""
+    url = get_url()
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -47,8 +41,9 @@ def run_migrations_offline() -> None:
 
 def run_migrations_online() -> None:
     """Run migrations in 'online' mode."""
+    url = get_url()
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        {"sqlalchemy.url": url},
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
